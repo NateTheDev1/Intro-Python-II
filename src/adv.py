111b713ce8618261c654d4a2fb04541a38915777
NateTheDev1 <nthnlrichards@gmail.com>
2020-08-04 23:11:28 -0400

    invetory system working

--- src/adv.py
+++ src/adv.py
@@ -43,21 +43,21 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Food
-apple = Item('Apple', 'Red and juicy.')
-steak = Item('Steak', 'Cold, but could help.')
+apple = Item('apple', 'Red and juicy.')
+steak = Item('steak', 'Cold, but could help.')
 
 # Weapon
-sword = Item('Iron Sword', 'It is incredibly shiny.')
+sword = Item('sword', 'It is incredibly shiny.')
 
 # Loot
-coin = Item('Gold Coin', 'Preciouss.. so shiny...')
-ruby = Item('Ruby', "Is it red because it's a ruby? Or is red from blood...")
+coin = Item('gold', 'Preciouss.. so shiny...')
+ruby = Item('ruby', "Is it red because it's a ruby? Or is red from blood...")
 
 items_list = [coin, ruby, sword, apple, steak]
 
 
 ## Initialize items
-# room['outside'].items = []
+
 roomList = ['narrow', 'outside', 'foyer', 'overlook', 'treasure']
 
 for i in items_list:
@@ -76,9 +76,22 @@ def getItemList(roomName):
     except AttributeError:
         print('No Items In This Room')
 
+
+
+    
+
+
 # Make a new player object that is currently in the 'outside' room.
 
 player1 = Player('outside')
+
+def addItem(roomName, itemName):
+    for i, item in enumerate(room[roomName].items):
+        if item.name == itemName:
+            player1.items.append(room[roomName].items[i])
+            room[roomName].items[i].on_take()
+            del room[roomName].items[i]
+            break
 
 
 # Write a loop that:
@@ -95,29 +108,46 @@ player1 = Player('outside')
 print(f"Current Room: {room[player1.location].name}")
 print(textwrap.TextWrapper(50).fill(f"Description: {room[player1.location].description}\n"))
 print('----------------------------------------------\n')
+print('To pickup an item: type [take or get] [itemname]')
+print('To drop an item: type [drop] [itemname]')
 print(f"Items In Room:")
 getItemList(player1.location)
 print('----------------------------------------------\n')
 
 
-user = input("[n] Move North   [w] Move West   [s] Move South   [e] Move East   [q] Quit\n \n")
+user = input("[i] Inventory   [n] Move North   [w] Move West   [s] Move South   [e] Move East   [q] Quit\n \n")
 
 
 while not user == 'q':
-    if user == 'n':
+    if user.split()[0] == 'take' or user.split()[0] == 'get':
+        try:
+            item = user.split()[1]
+            addItem(player1.location, item)
+            user = input("[i] Inventory   [n] Move North   [w] Move West   [s] Move South   [e] Move East   [q] Quit\n \n")
+        except IndexError:
+            print("What would you like to take?")
+            print("Missing ***[itemname]*** Please Try Again \n")
+            user = input("[i] Inventory   [n] Move North   [w] Move West   [s] Move South   [e] Move East   [q] Quit\n \n")
+    elif user == 'i':
+        player1._inventory_()
+        user = input("[i] Inventory   [n] Move North   [w] Move West   [s] Move South   [e] Move East   [q] Quit\n \n")
+    elif user == 'n':
         try:
             player1.location = room[player1.location].n_to.title
             print(f"Current Room: {room[player1.location].name}")
             print(textwrap.TextWrapper(50).fill(f"Description: {room[player1.location].description}\n"))
             print('----------------------------------------------\n')
+            print('To pickup an item: type [take or get] [itemname]')
+            print('To drop an item: type [drop] [itemname]')
+            print('----------------------------------------------\n')
             print(f"Items In Room:")
             getItemList(player1.location)
             print('----------------------------------------------\n')
-            user = input("[n] Move North   [w] Move Left   [s] Move Up   [e] Move Down   [q] Quit\n \n")
+            user = input("[i] Inventory   [n] Move North   [w] Move West   [s] Move South   [e] Move East   [q] Quit\n \n")
 
         except AttributeError:
             print("Movement not allowed")
-            user = input("[n] Move North   [w] Move Left   [s] Move Up   [e] Move Down   [q] Quit\n \n")
+            user = input("[i] Inventory   [n] Move North   [w] Move West   [s] Move South   [e] Move East   [q] Quit\n \n")
 
     elif user == 's':
         try:
@@ -125,44 +155,53 @@ while not user == 'q':
             print(f"Current Room: {room[player1.location].name}")
             print(textwrap.TextWrapper(50).fill(f"Description: {room[player1.location].description}\n"))
             print('----------------------------------------------\n')
+            print('To pickup an item: type [take or get] [itemname]')
+            print('To drop an item: type [drop] [itemname]')
+            print('----------------------------------------------\n')
             print(f"Items In Room:")
             getItemList(player1.location)
             print('----------------------------------------------\n')
-            user = input("[n] Move North   [w] Move Left   [s] Move Up   [e] Move Down   [q] Quit\n \n")
+            user = input("[i] Inventory   [n] Move North   [w] Move West   [s] Move South   [e] Move East   [q] Quit\n \n")
 
         except AttributeError:
             print("Movement not allowed")
-            user = input("[n] Move North   [w] Move Left   [s] Move Up   [e] Move Down   [q] Quit\n \n")
+            user = input("[i] Inventory   [n] Move North   [w] Move West   [s] Move South   [e] Move East   [q] Quit\n \n")
     elif user == 'w':
         try:
             player1.location = room[player1.location].w_to.title
             print(f"Current Room: {room[player1.location].name}")
             print(textwrap.TextWrapper(50).fill(f"Description: {room[player1.location].description}\n"))
             print('----------------------------------------------\n')
+            print('To pickup an item: type [take or get] [itemname]')
+            print('To drop an item: type [drop] [itemname]')
+            print('----------------------------------------------\n')
             print(f"Items In Room:")
             getItemList(player1.location)
             print('----------------------------------------------\n')
-            user = input("[n] Move North   [w] Move Left   [s] Move Up   [e] Move Down   [q] Quit\n \n")
+            user = input("[i] Inventory   [n] Move North   [w] Move West   [s] Move South   [e] Move East   [q] Quit\n \n")
 
         except AttributeError:
             print("Movement not allowed")
-            user = input("[n] Move North   [w] Move Left   [s] Move Up   [e] Move Down   [q] Quit\n \n")
+            user = input("[i] Inventory   [n] Move North   [w] Move West   [s] Move South   [e] Move East   [q] Quit\n \n")
     elif user == 'e':
         try:
             player1.location = room[player1.location].e_to.title
             print(f"Current Room: {room[player1.location].name}")
             print(textwrap.TextWrapper(50).fill(f"Description: {room[player1.location].description}\n"))
             print('----------------------------------------------\n')
+            print('To pickup an item: type [take or get] [itemname]')
+            print('To drop an item: type [drop] [itemname]')
+            print('----------------------------------------------\n')
             print(f"Items In Room:")
             getItemList(player1.location)
             print('----------------------------------------------\n')
-            user = input("[n] Move North   [w] Move Left   [s] Move Up   [e] Move Down   [q] Quit\n \n")
+            user = input("[i] Inventory   [n] Move North   [w] Move West   [s] Move South   [e] Move East   [q] Quit\n \n")
 
         except AttributeError:
             print("Movement not allowed")
-            user = input("[n] Move North   [w] Move Left   [s] Move Up   [e] Move Down   [q] Quit\n \n")
+            user = input("[i] Inventory   [n] Move North   [w] Move West   [s] Move South   [e] Move East   [q] Quit\n \n")
     else:
         print("Invalid selection. Please try again.")
-        user = input("[n] Move North   [w] Move Left   [s] Move Up   [e] Move Down   [q] Quit\n \n")
+        user = input("[i] Inventory   [n] Move North   [w] Move West   [s] Move South   [e] Move East   [q] Quit\n \n")
         
         
